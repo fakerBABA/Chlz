80eff0be32e5d954c3bbf27a17559d3b6025df92
fakerBABA <leesengd@foxmail.com>
2016-07-29 11:40:57 +0800

    提交built目录 及package.json

---
+++ src/common/calendar.js
@@ -0,0 +1,30 @@
+var sdate = require("sildate");
+
+function S (selecter) {
+  return document.querySelecter(selecter);
+}
+
+function calendar (selecter) {
+
+}
+
+calendar.fn = calendar.prototype;
+
+calendar.fn.init = function () {
+
+};
+
+/**
+* 根据容器生成格子
+*/
+calendar.fn.htmler = function () {
+
+};
+
+calendar.fn.getNowTime = function () {
+
+};
+
+calendar.fn.formatter = function () {
+
+};
