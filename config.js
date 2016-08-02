ac1a16b131b25a2d01c3d313fd71f6339d4913c0
ZhaoHuan <273484500@qq.com>
2016-08-02 10:46:51 +0800

    redis session

--- config.js
+++ config.js
@@ -4,7 +4,9 @@
 var _config = {
     port : 8080,
     host : 'localhost',
-    mongodb : 'mongodb://localhost/chlz'
+    mongodb : 'mongodb://localhost/chlz',
+    redisIP : '127.0.0.1',
+    redisPort : 6379
 };
 
 module.exports = _config;
